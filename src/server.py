546b9c7569c12d2156679bf9913667668535de94
digital verdigris <montgomery.austin00@gmail.com>
2025-03-07 20:07:40 -0800

    Revert "tls/ssl implementation"
    This reverts commit 2400d93883c1e7897cac917048a5ad62ee7cd88c.

--- src/server.py
+++ src/server.py
@@ -1,31 +1,22 @@
 import socket
-import ssl
+import threading
+import json
 
 class checkers_server:
-    def __init__(self, host = "0.0.0.0", port = 5000, certfile="keys/server_certificate.pem", keyfile="keys/server_private_key.pem"):
+    def __init__(self, host = "0.0.0.0", port = 5000):
         self.host = host
         self.port = port
-        self.certfile = certfile
-        self.keyfile = keyfile
         self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_sock = None
         self.client_addr = None
-
     
     def start_listener(self):
-        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-        
-        context.load_cert_chain(certfile=self.certfile, keyfile=self.keyfile)
-
         self.server_sock.bind((self.host, self.port))
         self.server_sock.listen(1)
         print(f"waiting for a client to connect on {self.host}:{self.port}...")
-    
         self.client_sock, self.client_addr = self.server_sock.accept()
-
-        self.client_sock = context.wrap_socket(self.client_sock, server_side=True)
         print(f"client {self.client_addr} connected!")
-
+    
     def send_move(self, move):
         if self.client_sock:
             self.client_sock.sendall(move.encode())
